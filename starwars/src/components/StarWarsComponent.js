471955974be90a2cc63e9ad5b5ab5f9d4ee8354b
Michael Williams <manicdesigner@gmail.com>
2019-08-31 14:17:42 -0400

    built out omponent for styling and holding all other pieces for starwars api

--- starwars/src/components/StarWarsComponent.js
+++ starwars/src/components/StarWarsComponent.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { StarWarsName } from './StarWarsName';
+import { StarWarsBirthYear } from './StarWarsBirthYear';
+import { StarWarsEyeColor } from './StarWarsEyeColor';
+import { StarWarsMass } from './StarWarsMass';
+import { CardColumns, Card } from 'reactstrap';
+import styled from 'styled-components';
 
-function StarWarsComponent(props) {
-	return <div>{/*  main container for all other components*/}</div>;
+export function StarWarsComponent(props) {
+	const { starWarsCharacters } = props;
+
+	return (
+		<CardColumns>
+			{starWarsCharacters.map((starWarsCharacter, index) => {
+				return (
+					<StarWarsCard>
+						<StarWarsName name={starWarsCharacter.name} key={index + 'n'} />
+						<StarWarsBirthYear birth_year={starWarsCharacter.birth_year} key={index + 'b'} />
+						<StarWarsEyeColor eye_color={starWarsCharacter.eye_color} key={index + 'e'} />
+						<StarWarsMass mass={starWarsCharacter.mass} key={index + 'm'} />
+					</StarWarsCard>
+				);
+			})}
+		</CardColumns>
+	);
 }
-export default StarWarsComponent;
+
+const StarWarsCard = styled(Card)`
+	margin: 15px 1.5% 25px;
+`;
